a772832970f87225c3ce1c29c439acf13252d06f
Kevin Becker <kevin.becker@senioroi.com>
2022-06-21 10:34:12 -0400

    Add export call

--- WelcomeHome.Sdk/IWelcomeHomeApiClient.cs
+++ WelcomeHome.Sdk/IWelcomeHomeApiClient.cs
@@ -1,4 +1,6 @@
-﻿using Refit;
+﻿using System.Runtime.Serialization;
+
+using Refit;
 
 using WelcomeHome.Sdk.Models;
 
@@ -6,11 +8,23 @@ namespace WelcomeHome.Sdk;
 
 public interface IWelcomeHomeApiClient
 {
+    [Get("/exports/community/{id}/table/{table}")]
+    Task<string> ExportAsync([AliasAs("id")] int communityId,
+                             [AliasAs("table")] Table table,
+                             [Authorize("Token")] string authorization,
+                             [AliasAs("limit")] int? limit = null,
+                             [AliasAs("sort_by")] SortBy sortBy = SortBy.UpdatedAt,
+                             [AliasAs("filters[created_at_after]")][Query(Format = "yyyy-MM-ddTHH:mm:ss.fffZ")] DateTimeOffset? createdAtAfterUtc = null,
+                             [AliasAs("filters[updated_at_after]")][Query(Format = "yyyy-MM-ddTHH:mm:ss.fffZ")] DateTimeOffset? updatedAtAfterUtc = null,
+                             [AliasAs("filters[created_at_before]")][Query(Format = "yyyy-MM-ddTHH:mm:ss.fffZ")] DateTimeOffset? createdAtBeforeUtc = null,
+                             [AliasAs("filters[updated_at_before]")][Query(Format = "yyyy-MM-ddTHH:mm:ss.fffZ")] DateTimeOffset? updatedAtBeforeUtc = null,
+                             CancellationToken cancellationToken = default);
+
+    [Get("/lead_sources")]
+    Task<IEnumerable<ListLeadSourcesResponse>> ListLeadSources([Authorize("Token")] string authorization, CancellationToken cancellationToken = default!);
+
     [Get("/ping")]
     Task<PingResponse> PingAsync([Authorize("Token")] string authorization, CancellationToken cancellationToken = default);
-
-    [Get("/traits")]
-    Task<IEnumerable<TraitsResponse>> TraitsAsync([Authorize("Token")] string authorization, CancellationToken cancellationToken = default);
 
     [Post("/prospects")]
     Task<ProspectResponse> AddProspectAsync([Body(BodySerializationMethod.UrlEncoded)] ProspectRequest prospectRequest, [Authorize("Token")] string authorization, CancellationToken cancellationToken = default);
@@ -23,4 +37,35 @@ public interface IWelcomeHomeApiClient
 
     [Put("/residents/{id}")]
     Task<ResidentResponse> UpdateResidentAsync([AliasAs("id")] int residentId, [Body(BodySerializationMethod.Serialized)] ResidentRequest residentRequest, [Authorize("Token")] string authorization, CancellationToken cancellationToken = default);
+
+    [Get("/traits")]
+    Task<IEnumerable<TraitsResponse>> TraitsAsync([Authorize("Token")] string authorization, CancellationToken cancellationToken = default);
+}
+
+public enum SortBy
+{
+    [EnumMember(Value = "updated_at")]
+    UpdatedAt,
+    [EnumMember(Value = "created_at")]
+    CreatedAt,
+}
+
+public enum Table
+{
+    [EnumMember(Value = "Organizations")]
+    Organizations,
+    [EnumMember(Value = "Referrers")]
+    Referrers,
+    [EnumMember(Value = "Prospects")]
+    Prospects,
+    [EnumMember(Value = "Residents")]
+    Residents,
+    [EnumMember(Value = "Influencers")]
+    Influencers,
+    [EnumMember(Value = "Activities")]
+    Activities,
+    [EnumMember(Value = "Units")]
+    Units,
+    [EnumMember(Value = "HousingContracts")]
+    HousingContracts
 }
---
+++ WelcomeHome.Sdk/Models/ListLeadSourcesResponse.cs
@@ -0,0 +1,25 @@
+﻿
+using Newtonsoft.Json;
+
+namespace WelcomeHome.Sdk.Models;
+
+public class ListLeadSourcesResponse
+{
+    [JsonProperty(PropertyName = "id")]
+    public int Id { get; set; }
+
+    [JsonProperty(PropertyName = "name")]
+    public string Name { get; set; } = default!;
+
+    [JsonProperty(PropertyName = "category")]
+    public string Category { get; set; } = default!;
+
+    [JsonProperty(PropertyName = "discarded_at")]
+    public DateTime? DiscardedAt { get; set; }
+
+    [JsonProperty(PropertyName = "created_at")]
+    public DateTime? CreatedAt { get; set; }
+
+    [JsonProperty(PropertyName = "updated_at")]
+    public DateTime? UpdatedAt { get; set; }
+}
